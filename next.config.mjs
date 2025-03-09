a79bba748304693b36af57a2bc9238c66555ee35
tsots <mosalatsotetsi@gmail.com>
2025-03-09 16:58:38 +0200

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/mynext",
+  output: "export",
+  reactStrictMode: true,
+};
+
+export default nextConfig;
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Let us try again!</h1>
+      <p>Nothing is happening!</p>
+    </>
+  );
+}
